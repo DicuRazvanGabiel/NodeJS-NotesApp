2b147345096a0b142b97684420369e447c6c2a11
Dicu Razvan Gabriel <razvan.dicu@itsix.com>
2019-10-16 19:07:55 +0300

    To the router

--- task-app/src/db/mongoose.js
+++ task-app/src/db/mongoose.js
@@ -1,50 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
-});
-
-const User = mongoose.model('User', {
-    name: {
-        type: String
-    },
-
-    age: {
-        type: Number
-    }
-});
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
-    },
-
-    completed: {
-        type: Boolean
-    }
-});
-
-const task1 = new Task({
-    description: 'First task',
-    completed: false
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true
 })
-
-task1.save().then(() => {
-    console.log(task1);
-    
-}).catch((error) => {
-    console.log(error);
-    
-})
-
-// const me = new User({
-//     name: 'Razvan',
-//     age: 22
-// });
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log(error);
-// });
